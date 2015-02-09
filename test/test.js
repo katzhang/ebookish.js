0f2b068ba6d37c8068a3aa2c357b6c9685cef193
katzhang-ias <arcadychang@gmail.com>
2015-02-08 20:38:43 -0500

    added unit test for argvParser

--- test/test.js
+++ test/test.js
@@ -1,7 +1,17 @@
 var ebookish = require('../index');
-var argvParser require('../lib/argvParser');
-var expect = require('chai').expect;
+var argvParser = require('../lib/argvParser');
+var assert = require('assert');
 
 describe('argvParser', function() {
-	
+	it('can parse argv with command and options', function() {
+		var argv = ['node', 'xx.js', 'ls', '--all'];
+		var parsedObj = argvParser(argv);
+		console.log(parsedObj);
+		assert.deepEqual(parsedObj, {
+			command: 'ls',
+			option: 'all',
+			argument: undefined
+		})
+
+	});
 });
