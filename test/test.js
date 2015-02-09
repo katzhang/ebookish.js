f01f1eed74d7607de7f86a936e8f35f7745dbae9
katzhang-ias <arcadychang@gmail.com>
2015-02-08 20:43:48 -0500

    removed verifying file existence part from argvParser

--- test/test.js
+++ test/test.js
@@ -3,15 +3,27 @@ var argvParser = require('../lib/argvParser');
 var assert = require('assert');
 
 describe('argvParser', function() {
-	it('can parse argv with command and options', function() {
-		var argv = ['node', 'xx.js', 'ls', '--all'];
+	it('can parse argv with command without options or argument', function() {
+		var argv = ['node', 'xx.js', 'ls'];
 		var parsedObj = argvParser(argv);
-		console.log(parsedObj);
+
 		assert.deepEqual(parsedObj, {
 			command: 'ls',
-			option: 'all',
+			option: undefined,
 			argument: undefined
 		})
 
 	});
+
+	it('can parse argv with command and options and argument', function() {
+		var argv = ['node', 'xx.js', 'ls', 'book-1.pdf', '--all'];
+		var parsedObj = argvParser(argv);
+
+		assert.deepEqual(parsedObj, {
+			command: 'ls',
+			option: 'all',
+			argument: 'book-1.pdf'
+		})
+	});
 });
+
