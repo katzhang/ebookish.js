8df364d2598adc13ef8973b4a6e0533341c7b14b
katzhang-ias <arcadychang@gmail.com>
2015-02-09 23:25:05 -0500

    added cli in index.js

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 var fs = require('fs');
 var path = require('path');
-var dir = process.argv[2] || __dirname.toString();
+var argvParser = require('./lib/argvParser');
 var ebookish = module.exports;
 
-//list all pdf files
-ebookish.ls = function() {
-	fs.readdir(dir, function(err, files) {
+
+//list [all] df files
+var ls = ebookish.ls = function(option, dir) {
+	var output = [];
+	fs.readdir(dir || '.', function(err, files) {
 		if (err) {
 			throw 'error!';
 		} else {
 			files.forEach(function(file, index, array) {
 				if (path.extname(file) === '.pdf') {
-					console.log(file);
+					output.push(file);
 				}
 			});
 		}
+
+		return output;
 	});
+
 };
+
+
+
+var runCli = ebookish.runCli = function(args) {
+	var fn = ebookish[args.command];
+	fn.apply(null, [args.option, args.directory]);
+};
+
+if (require.main === module) {
+	try {
+		runCli(argvParser(process.argv));
+	} catch(e) {
+		console.error(e);
+	}
+}
+
